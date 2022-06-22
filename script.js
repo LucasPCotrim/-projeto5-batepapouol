99747e752cfbcd5aec31b3f1925474a252cc7b13
lucas_cotrim <lucas_cotrim@tpn.usp.br>
2022-06-22 01:42:05 -0300

    Implemented 5s ping to keep user in chat

--- script.js
+++ script.js
@@ -2,6 +2,8 @@
 let username;
 let typed_username;
 const participants_url = 'https://mock-api.driven.com.br/api/v6/uol/participants ';
+const status_url = 'https://mock-api.driven.com.br/api/v6/uol/status';
+let ping_time_interval;
 
 let DOM_entry_screen = document.querySelector('.entry_screen');
 let DOM_username_input = document.querySelector('.entry_screen input');
@@ -16,6 +18,7 @@ function SERVER_process_username_answer(answer) {
     if(answer.status == 200){
         console.log('status == 200');
         username = typed_username;
+        start_ping_server_interval();
         DOM_entry_screen.classList.toggle('hidden');
         DOM_loading_screen.classList.toggle('hidden');
         setTimeout(function(){ DOM_loading_screen.classList.toggle('hidden');
@@ -31,9 +34,9 @@ function SERVER_process_username_error(error){
         console.log('status == 400');
         console.log('This Username is already taken');
         DOM_invalid_user_name_popup.style.opacity = 0.6;
-        DOM_invalid_user_name_popup.innerHTML = "This Username is already taken!";
+        DOM_invalid_user_name_popup.innerHTML = "Nome já está em uso! Digite outro";
         setTimeout(function(){ DOM_invalid_user_name_popup.style.opacity = 0;
-                               DOM_invalid_user_name_popup.innerHTML = ""; }, 1500);
+                               DOM_invalid_user_name_popup.innerHTML = ""; }, 2000);
     }
 }
 function log_in(){
@@ -43,9 +46,9 @@ function log_in(){
     if (typed_username.trim().length === 0 || !String(typed_username)){
         console.log('Please insert a valid name');
         DOM_invalid_user_name_popup.style.opacity = 0.6;
-        DOM_invalid_user_name_popup.innerHTML = "Please insert a valid Usename!"
+        DOM_invalid_user_name_popup.innerHTML = "Insira um nome de usuário válido"
         setTimeout(function(){ DOM_invalid_user_name_popup.style.opacity = 0;
-                               DOM_invalid_user_name_popup.innerHTML = ""; }, 1500);
+                               DOM_invalid_user_name_popup.innerHTML = ""; }, 2000);
     }
     else {
         DOM_invalid_user_name_popup.style.opacity = 0;
@@ -62,11 +65,20 @@ function log_in(){
                                      .catch(SERVER_process_username_error);
     }
 
-    
-
 }
 
-
+function start_ping_server_interval(){
+    ping_time_interval = setInterval(function () {
+                                        axios({
+                                            method: 'post',
+                                            url: status_url,
+                                            data: {
+                                            name: username,
+                                            }
+                                        });
+                                        console.log('ping')
+                                     }, 5000);
+}
 
 
 
